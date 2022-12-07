220286f148aaa15ef3f7005a774a37e9d953c257
Sai <s20mouni@gmail.com>
2022-12-07 13:03:17 +0530

    new commit

--- MathCalculator/Program.cs
+++ MathCalculator/Program.cs
@@ -1,4 +1,7 @@
 
+using MathCalculator;
+using Math = MathCalculator.Math;
+
 var builder = WebApplication.CreateBuilder(args);
 //var app = builder.Build();
 
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IMath, Math>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
